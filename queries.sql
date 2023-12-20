b6f0a936a456018d770cab1921e63f2e737289e1
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-20 15:34:54 +0100

    Add: second query, fix: first count query, discarding nested query as unnecessary for the count

--- queries.sql
+++ queries.sql
@@ -36,13 +36,27 @@ ORDER BY
 SELECT
     COUNT(*) AS total_students
 FROM
-    (
-        SELECT
-            *
-        FROM
-            students
-        WHERE
-            YEAR(date_of_birth) = 1990
-    ) AS students_born_in_1990;
+    students
+WHERE
+    YEAR(date_of_birth) = 1990;
 
 --2) Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+--estrapolare tutti i dati dei corsi con più di 10 cfu:
+SELECT
+    *
+FROM
+    `courses`
+WHERE
+    `cfu` > 10;
+
+--contare tutti i corsi con più di 10 cfu
+
+SELECT
+    COUNT(*)
+FROM
+    `courses`
+WHERE
+    `cfu` > 10;
+
+-- 
