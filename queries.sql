171b44531ef4b95f07b3d857361d52c6fda1f25f
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-20 16:46:51 +0100

    Add: sixth query

--- queries.sql
+++ queries.sql
@@ -126,3 +126,41 @@ WHERE
     AND period = 'I semestre';
 
 --5. 5.Selezionare tutti gli appelli d 'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+--estrapolazione dati
+SELECT
+    *
+FROM
+    `exams`
+WHERE
+    `date` = '2020-06-20'
+    AND `hour` > '14:00:00';
+
+--conto 
+SELECT
+    COUNT(*)
+FROM
+    `exams`
+WHERE
+    `date` = '2020-06-20'
+    AND `hour` > '14:00:00';
+
+--6.Selezionare tutti i corsi di laurea magistrale (38)
+-- estrapolare dati ordinandili in ordine alfabetico x nomr
+
+SELECT
+    *
+FROM
+    `degrees`
+WHERE
+    `level` = 'magistrale'
+ORDER BY
+    `name` ASC;
+--contare il totale
+SELECT
+    COUNT(*)
+FROM
+    `degrees`
+WHERE
+    `level` = 'magistrale';
+
+--7 Da quanti dipartimenti è composta l 'università? (12)
